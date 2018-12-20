7d0c782375059feae9b2f5a0f5389df2964596f2
Damien <dfran>
2018-12-20 19:39:26 +0100

    1st release

---
+++ VcsClass.py
@@ -0,0 +1,21 @@
+import logging
+
+class VcsClass :
+	"""Class to describe Incoming Parameters of a CPL from a Cisco VCS 
+	"""
+
+	def __init__(self, parameter):
+		self.logger = logging.getLogger('')
+		self.__parameter = str(parameter)
+		print listOutput
+
+
+	def getData(self, rank):
+	"""Method Share by some other method to get the required substring
+	"""
+		message = request.get_data(cache=False)
+		message = message.split("&")
+		return message[rank] 	
+	
+
+	def ALLOW_INTERWORKING
