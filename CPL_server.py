f0f8b8ff351f077e6c4d55ad8d1f8ca7c693d80a
Damien <dfran>
2018-12-20 19:40:18 +0100

    modified

--- CPL_server.py
+++ CPL_server.py
@@ -1,5 +1,6 @@
 #! /usr/bin/python
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+#from Classes import VcsClass
 
 app = Flask(__name__)
 
@@ -11,13 +12,30 @@ def index():
 def damien():
 	return "Damien"
 
-@app.route ('/allow')
-def allow():
-	return render_template('allow_cpl')
+@app.route('/status',methods=['GET'])
+def status():
+	return "Server is UP!!!"
 
-@app.route('/allow-frame')
+@app.route ('/allow',methods=['GET','POST'])
 def allowframe():
 	return render_template('allow_cpl_frame')
+
+@app.route ('/deny',methods=['POST'])
+def deny():
+	return render_template('reject_cpl')
+
+@app.route ('/partners',methods=['POST'])
+def partners():
+	
+	data = request.get_data(cache=False)
+	#app.logger.debug(data)
+	split = data.split("&")
+	app.logger.debug(split[15])
+	return "0"
+
+
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5000,debug=True)
