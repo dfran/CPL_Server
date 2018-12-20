2066d221758c72b2319ce0c17a61eafaf4b815f3
dfran <hello@damienfran.com>
2018-12-20 23:10:05 +0100

    modified class

--- VcsClass.py
+++ VcsClass.py
@@ -7,6 +7,8 @@ class VcsClass :
 	def __init__(self, parameter):
 		self.logger = logging.getLogger('')
 		self.__parameter = str(parameter)
+		if :
+			pass
 		print listOutput
 
 
@@ -15,7 +17,113 @@ class VcsClass :
 	"""
 		message = request.get_data(cache=False)
 		message = message.split("&")
-		return message[rank] 	
+		value = message[rank].split("=")
+		return value[2] 	
 	
 
-	def ALLOW_INTERWORKING
+	def ALLOW_INTERWORKING(self):
+	"""Value is TRUE/False
+	"""
+		listOutput = self.getdata(0)
+		return listOutput
+
+	def AUTHENTICATED(self):
+	"""Value is TRUE/False
+	"""	
+		listOutput = self.getdata(1)
+		return listOutput
+
+	def AUTHENTICATED_SOURCE_ALIAS(self):
+		if self.AUTHENTICATED == "TRUE":
+			listOutput = self.getData(2)
+		else:
+			listOutput = "User Not AUTHENTICATED"
+		return listOutput
+
+	def AUTHENTICATION_USER_NAME(self):
+		if self.AUTHENTICATED == "TRUE":
+			listOutput = self.getData(3)
+		else:
+			listOutput = self.getdata(2)
+		return listOutput
+
+	def CLUSTER_NAME(self):
+		if self.AUTHENTICATED == "TRUE":
+			listOutput = self.getdata(4)
+		else:
+			listOutput = self.getdata(3)	
+		return listOutput
+
+	def DESTINATION_ALIAS(self):
+		listOutput = self.getdata(4)
+		return listOutput
+
+	def DESTINATION_ALIAS_PARAMS(self):
+		listOutput = self.getdata(5)
+		return listOutput
+
+	def GLOBAL_CALL_SERIAL_NUMBER(self):
+		listOutput = self.getdata(6)
+		return listOutput
+
+	def LOCAL_CALL_SERIAL_NUMBER(self):
+		listOutput = self.getdata(7)
+		return listOutput
+
+	def METHOD(self):
+	"""Value is INVITE / ARQ / LRQ / OPTIONS / SETUP
+	"""	
+		listOutput = self.getdata(9)
+		return listOutput
+
+	def NETWORK_TYPE(self):
+	"""Value is IPV4 / IPV6
+	"""	
+		listOutput = self.getdata(10)
+		return listOutput
+
+	def POLICY_TYPE(self):
+	"""Value is SEARCH / ADMIN
+	"""
+		listOutput = self.getdata(11)
+		return listOutput
+
+	def PROTOCOL(self):
+	"""Value is SIP / H323
+	"""
+		listOutput = self.getdata(12)
+		return listOutput
+
+	def REGISTERED_ALIAS(self):
+		listOutput = self.getdata(12)
+		return listOutput
+
+	def SOURCE_ADDRESS(self):
+		listOutput = self.getdata(0)
+		return listOutput
+
+	def SOURCE_IP(self):
+		listOutput = self.getdata(0)
+		return listOutput
+
+	def SOURCE_PORT(self):
+		listOutput = self.getdata(0)
+		return listOutput
+
+	def TRAVERSAL_TYPE(self):
+	"""Value is TYPE_[UNDEF / ASSENTSERVER / ASSENTCLIENT /	H460SERVER / H460CLIENT / TURNSERVER / TURNCLIENT / ICE]
+	"""
+		listOutput = self.getdata(0)
+		return listOutput
+		
+	def UNAUTHENTICATED_SOURCE_ALIAS(self):
+		listOutput = self.getdata(0)
+		return listOutput
+
+	def UTCTIME(self):
+		listOutput = self.getdata(0)
+		return listOutput
+
+	def ZONE_NAME(self):
+		listOutput = self.getdata(0)
+		return listOutput
