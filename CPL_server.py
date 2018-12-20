bcc5d84275e4513e0712a9e24928bca3748d292d
Damien <dfran>
2018-12-20 13:58:24 +0100

    Jinja add-in

--- CPL_server.py
+++ CPL_server.py
@@ -1,5 +1,5 @@
 #! /usr/bin/python
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -10,6 +10,14 @@ def index():
 @app.route('/Damien')
 def damien():
 	return "Damien"
+
+@app.route ('/allow')
+def allow():
+	return render_template('allow_cpl')
+
+@app.route('/allow-frame')
+def allowframe():
+	return render_template('allow_cpl_frame')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5000,debug=True)
