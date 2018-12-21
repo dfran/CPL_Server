a31e6089bad1992cd29e87c9a648035ac7f9fc2e
Damien <dfran>
2018-12-21 16:21:30 +0100

    Add CallPolicy rules

--- CPL_server.py
+++ CPL_server.py
@@ -2,6 +2,10 @@
 from flask import Flask, render_template, request
 #from Classes import VcsClass
 
+trusted_partners = ["dimensiondata.call.ciscospark.com"]
+internal_domains = ["labddfr.com"]
+callable_domains = ["pmr.labddfr.com"]
+
 app = Flask(__name__)
 
 @app.route('/',methods=['GET'])
@@ -28,14 +32,44 @@ def deny():
 def partners():
 	
 	data = request.get_data(cache=False)
-	#app.logger.debug(data)
-	split = data.split("&")
-	app.logger.debug(split[15])
+	#ap.logger.debug(data)
+	#split = data.split("&")
+	app.logger.debug(data)
+	
+	my_list = [tuple(i.split('=')) for i in data.split('&')]
+	my_dict = dict(my_list)
+	
+	
+	app.logger.debug(my_dict["UNAUTHENTICATED_SOURCE_ALIAS"].split("%40")[1])
+
+	
 	return "0"
 
+@app.route('/callpolicy',methods=['POST'])
+def callpolicy():
 
+        data = request.get_data(cache=False)
+        my_list = [tuple(i.split("=")) for i in data.split('&')]
+        my_dict = dict(my_list)
+	
+	app.logger.debug(data)
+		
+	if my_dict["AUTHENTICATED"] == "FALSE": 
+		if my_dict["UNAUTHENTICATED_SOURCE_ALIAS"].split("%40")[1] in trusted_partners or my_dict["DESTINATION_ALIAS"].split("%40")[1] in callable_domains:
+			
+			app.logger.debug("Incoming Call Allowed")
+			return render_template("allow_cpl_frame")
+			
 
-
+	elif my_dict["AUTHENTICATED_SOURCE_ALIAS"].split("%40")[1] in internal_domains:
+		
+		app.logger.debug("Call to External Allowed")
+		return render_template("allow_cpl_frame")
+			
+	
+	app.logger.debug("Call Refused")
+	return render_template("reject_cpl")
+		
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5000,debug=True)
